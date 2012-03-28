2773d31e7be2497ed756bb122cc1d411a3bc6f8c
Robin <bussenot.robin@gmail.com>
2012-03-28 17:00:37 +0200

    generatoion code pour membre

--- FOM/grails-app/domain/fom/Membre.groovy
+++ FOM/grails-app/domain/fom/Membre.groovy
@@ -2,6 +2,15 @@ package fom
 
 class Membre {
 
+    
+    String nom
+    String prenom
+    String email
+    
+    
     static constraints = {
+        nom blank:false
+        prenom blank:false
+        email email:true,blank:false
     }
 }
